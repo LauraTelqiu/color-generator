f7d074fabd3e88c1709eac373a95ec1a1c83ae0a
Laura <lauratelqiu@gmail.com>
2023-05-08 20:07:31 -0400

    form

--- src/components/From.js
+++ src/components/From.js
@@ -1,31 +1,11 @@
-import { useState } from 'react';
+import React from 'react'
 
-const Form = ({ addColor }) => {
-  const [color, setColor] = useState('');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addColor(color);
-  };
+const From = () => {
   return (
-    <section className='container'>
-      <h4>color generator</h4>
-      <form className='color-form' onSubmit={handleSubmit}>
-        <input
-          type='color'
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-        />
-        <input
-          type='text'
-          value={color}
-          onChange={(e) => setColor(e.target.value)}
-          placeholder='#f15025'
-        />
-        <button className='btn' type='submit' style={{ background: color }}>
-          submit
-        </button>
-      </form>
-    </section>
-  );
-};
-export default Form;
+    <div>
+      From
+    </div>
+  )
+}
+
+export default From
