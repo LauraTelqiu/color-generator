df9abfcc419ca861abddfc8782be5c91a5648ae1
Laura <lauratelqiu@gmail.com>
2023-05-09 13:35:50 -0400

    render list

--- src/components/SingleColor.js
+++ src/components/SingleColor.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const SingleColor = () => {
+const SingleColor = ({ index, color }) => {
+  const { hex, weight } = color;
   return (
-    <div>
-      Single Color
-    </div>
+    <article
+      className={index > 10 ? 'color color-light' : 'color'}
+      style={{ background: `#${hex}` }}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>#{hex}</p>
+
+    </article >
   )
 }
 
