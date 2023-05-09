7473906624ff0780eda529bfa0f23b57b64d5440
Laura <lauratelqiu@gmail.com>
2023-05-09 13:13:37 -0400

    values.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import ColorList from './components/ColorList'
-import From from './components/From'
+import { useState } from "react";
+import ColorList from "./components/ColorList";
+import Form from "./components/Form";
+import Values from "values.js";
 
 const App = () => {
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+
   return (
-    <div>
-      <From />
+    <main>
+      <Form />
       <ColorList />
-    </div>
-  )
-}
+    </main>
+  );
+};
 
-export default App
-
+export default App;
